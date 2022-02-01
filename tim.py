6afdf6cfccc40fc0c6f8eb217c1e82cbafd65600
kjain <75043245+Krish-sysadmin@users.noreply.github.com>
2022-02-01 23:17:23 +0100

    Update tim.py

--- tim.py
+++ tim.py
@@ -1,37 +1,54 @@
-import os, sys, matplotlib
+from cProfile import label
+import os
+import sys
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import ones,vstack
+from numpy import ones, vstack
 from numpy.linalg import lstsq
 import scipy
+from scipy.optimize import curve_fit
 
-points = [(6, 2.907),(29 ,0.257)]
-x_coords_steeper, y_coords_steeper = zip(*points)
-A = vstack([x_coords_steeper,ones(len(x_coords_steeper))]).T
-m, c = lstsq(A, y_coords_steeper)[0]
-print("Line Solution is y = {m}x + {c}".format(m=m,c=c))
+
+def func(x, a):
+    return a * x
+
 
 xpoints = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 1])
 ypoints = np.array([0, 0.075, 0.132, 0.158, 0.194, 0.214, 0.464])
 
-steeperCurvexpoints = np.array([0, 0])
-steeperCurveypoints = np.array([0.989,0.464])
+xpointsWithoutNumpy = (0, 0.1, 0.2, 0.3, 0.4, 0.5, 1)
+yPointsWithoutNumpy = (0, 0.075, 0.132, 0.158, 0.194, 0.214, 0.464)
 
-shallowerCurvexpoints = np.array([0, 0])
-shallowerCurveypoints = np.array([1.011,0.464])
+popt, pcov = curve_fit(func, xpointsWithoutNumpy, yPointsWithoutNumpy)
 
-plt.plot(xpoints, ypoints, label = "Line of Best Fit (with R² = 1)")
+
+# FOR STEEPER AND SHALLOWER CURVES
+steeperCurvexpoints = np.array([0, 0.989])
+steeperCurveypoints = np.array([0, 0.464])
+
+shallowerCurvexpoints = np.array([0, 1.011])
+shallowerCurveypoints = np.array([0, 0.464])
+
+
+# plt.scatter(xpointsWithoutNumpy, yPointsWithoutNumpy, label="Calibration curve")
+plt.plot(xpointsWithoutNumpy, func(xpointsWithoutNumpy, popt),
+         "r--", label="Calibration curve y = 0.4709X")
+
+
 plt.title("Absorbance dependent on coffee concentration")
 plt.xlabel("concentration of caffeine ")
 plt.ylabel("Absorbance (275 Au)")
-plt.plot(steeperCurvexpoints, steeperCurveypoints, label = "steeper = " + "Ln(V) = -0.098T + 3.300" )
-plt.plot(shallowerCurvexpoints, shallowerCurveypoints, label = "shallower = " + " Ln(V) = -0.115T + 3.598")
+plt.plot(steeperCurvexpoints, steeperCurveypoints,
+         label="steeper = y=0.4691607684529828x")
+plt.plot(shallowerCurvexpoints, shallowerCurveypoints,
+         label="shallower = y=0.45895153313550946x")
 plt.grid()
 plt.legend()
 
-plt.errorbar(xpoints, ypoints, xerr=xpoints*0.011 , fmt='.k')
+plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+plt.yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+
+plt.errorbar(xpoints, ypoints, xerr=xpoints*0.011, fmt='.k')
 
 plt.show()
-
-
- 
